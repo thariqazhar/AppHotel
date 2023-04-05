1da48fd891388eff194a537547c39a62e8650056
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-04-05 10:01:26 +0700

    Input case 1

--- AppHotel/Program.cs
+++ AppHotel/Program.cs
@@ -28,10 +28,77 @@ namespace AppHotel
                     char chr = Convert.ToChar(Console.ReadLine());
                     switch (chr)
                     {
-
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data source =LAPTOP-9VURLJFC\\THARIQ_AZHAR; " +
+                                    "initial catalog = {0}; " +
+                                    "User ID = {1}; password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\nMenu");
+                                        Console.WriteLine("1. Insert data");
+                                        Console.WriteLine("2. Update data");
+                                        Console.WriteLine("3. Delete");
+                                        Console.WriteLine("4. Quit");
+                                        Console.WriteLine("\nEnter your choice (1-4): ");
+                                        char ch = Convert.ToChar(Console.ReadLine());
+                                        switch (ch)
+                                        {
+                                            case '1':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("Input Data Pelanggan\n");
+                                                    Console.WriteLine("Masukkan Id_Pelanggan:");
+                                                    string id_pelanggan = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Nama Pelanggan :");
+                                                    string nama_pelanggan = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan No. Telp :");
+                                                    string tlp_pelanggan = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan jenis kelamin (L/P) :");
+                                                    string jk = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan No. Kamar :");
+                                                    string no_kamar = Console.ReadLine();
+                                                    try
+                                                    {
+                                                        PR.insert(id_pelanggan, nama_pelanggan, tlp_pelanggan, jk, no_kamar, conn);
+                                                        conn.Close();
+                                                    }
+                                                    catch
+                                                    {
+                                                        Console.WriteLine("\nAnda tidak memiliki " + "akses untuk menambah data");
+                                                    }
+                                                }
+                                                break;
+                                            
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("\nCheck for the value entered.");
+                                    }
+                                }
+                            }
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
                     }
                 }
-             }
+                catch
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Tidak dapat mengakses database menggunakan user tersebut\n");
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
