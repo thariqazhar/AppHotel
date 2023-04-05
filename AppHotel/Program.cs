b9dda2f877473eabcb2e372c1bd4e2a5247c7294
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-04-05 09:58:44 +0700

    Membuat Menu

--- AppHotel/Program.cs
+++ AppHotel/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AppHotel
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Program PR = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database\n");
+                    Console.WriteLine("Masukkan User ID: ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password: ");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database tujuan: ");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+
+                    }
+                }
+             }
         }
     }
 }
+
